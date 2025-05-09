e735eb4df1a385ec71175fcd88b1b0fe63311519
bagoongyoo <badventure034>
2025-05-09 17:08:38 +0700

    praktikum

---
+++ vue-project/src/views/Experience.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="container my-5" data-aos="fade-down">
+    <h2 class="h3 mb-4 text-primary">Pengalaman</h2>
+    <div class="row g-4">
+      <!-- Card 1 -->
+      <div class="col-md-10">
+        <div class="card h-100 shadow-sm">
+          <div class="card-body">
+            <h5 class="card-title">Saya aw aw</h5>
+            <p class="card-text">
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
